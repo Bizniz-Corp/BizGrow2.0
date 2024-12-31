315b27b8e01254334040b412394274e5af04b38a
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-01-01 04:33:10 +0700

    [FIX] integrasi dari format html ke format laravel
    Signed-off-by: Ricky Renaldi <109163100+rickrck@users.noreply.github.com>

--- BizGrow-Web/resources/views/profil/profile_edit.blade.php
+++ BizGrow-Web/resources/views/profil/profile_edit.blade.php
@@ -10,8 +10,8 @@
       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
       crossorigin="anonymous"
     />
-    <link href="../assets/css/style.css" rel="stylesheet" />
-    <link href="../assets/css/profile.css" rel="stylesheet" />
+    <link href="{{ asset("css/style.css") }}" rel="stylesheet" />
+    <link href="{{ asset("css/profile.css") }}" rel="stylesheet" />
   </head>
 
   <body>
@@ -20,7 +20,7 @@
       <aside id="sidebar">
         <div class="d-flex flex-column mt-3">
           <button class="toggle-btn" type="button">
-            <img src="../img/Boom Es.jpeg" alt="profile_pict">
+            <img src="{{ asset("images/Boom Es.jpeg") }}" alt="profile_pict">
           </button>
           <div class="sidebar-nama text-center">
             <a href="#">Nama UMKM</a>
@@ -29,14 +29,14 @@
         <ul class="sidebar-nav">
           <li class="sidebar-item">
             <a href="indexhome.html" class="sidebar-link">
-              <img src="../img/keyboard.svg" alt="moneyicon">
+              <img src="{{ asset("images/keyboard.svg") }}" alt="moneyicon">
               <span>Beranda</span>
             </a>
           </li>
           <li class="sidebar-item">
             <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#penjualan"
               aria-expanded="true" aria-controls="penjualan">
-              <img src="../img/money-send.svg" class="money-icon">
+              <img src="{{ asset("images/money-send.svg") }}" class="money-icon">
               <span>Penjualan</span>
             </a>
             <ul class="sidebar-dropdown list-unstyled collapse" id="penjualan" data-bs-parent="#penjualan">
@@ -57,7 +57,7 @@
           <li class="sidebar-item">
             <a href="#" class="sidebar-link has-dropdown collapsed" data-bs-toggle="collapse" data-bs-target="#stok"
               aria-expanded="true" aria-controls="stok">
-              <img src="../img/box.svg" alt="iconbox">
+              <img src="{{ asset("images/box.svg") }}" alt="iconbox">
               <span>Stok</span>
             </a>
             <ul class="sidebar-dropdown list-unstyled collapse" id="stok" data-bs-parent="#stok">
@@ -74,14 +74,14 @@
           </li>
           <li class="sidebar-item">
             <a href="../pages/profile.html" class="sidebar-link">
-              <img src="../img/profile.svg" alt="iconprofile">
+              <img src="{{ asset("images/profil/profile.svg") }}" alt="iconprofile">
               <span>Profil</span>
             </a>
           </li>
         </ul>
         <div class="sidebar-footer">
           <a href="../index.html" class="sidebar-link">
-            <img src="../img/logout.svg" alt="iconprofile">
+            <img src="{{ asset("images/profil/logout.svg") }}" alt="iconprofile">
             <span>Keluar</span>
           </a>
         </div>
@@ -92,14 +92,14 @@
           class="p-3 d-flex justify-content-between align-items-center sticky-top"
         >
           <h3 class="h3 fw-bold">Edit Profil</h3>
-          <img src="../img/logo.png" alt="bizgrowlogo" />
+          <img src="{{ asset("images/logo.png") }}" alt="bizgrowlogo" />
         </header>
 
         <div class="filter-container m-5 d-flex align-items-start">
           <!-- Foto Profil -->
           <div class="position-relative">
             <img
-              src="../img/Boom Es.jpeg"
+              src="{{ asset("images/Boom Es.jpeg") }}"
               class="rounded-circle mb-3 profile-photo"
               alt="Avatar"
             />
@@ -108,7 +108,7 @@
               onclick="document.getElementById('fileInput').click()"
             >
               <img
-                src="../img/camera.svg"
+                src="{{ asset("images/camera.svg") }}"
                 alt="Edit Photo"
                 style="width: 20px; height: 20px"
               />
@@ -130,7 +130,7 @@
                 class="input-group-text position-absolute right-icon"
                 id="basic-addon2"
               >
-                <img src="../img/pencil.svg" alt="Edit" style="width: 16px" />
+                <img src="{{ asset("images/profil/pencil.svg") }}" alt="Edit" style="width: 16px" />
               </span>
             </div>
 
@@ -141,7 +141,7 @@
                 class="input-group-text position-absolute right-icon"
                 id="basic-addon2"
               >
-                <img src="../img/pencil.svg" alt="Email" style="width: 16px" />
+                <img src="{{ asset("images/profil/pencil.svg") }}" alt="Email" style="width: 16px" />
               </span>
             </div>
 
@@ -155,7 +155,7 @@
                 title="Kamu tidak bisa mengganti NPWP, hubungi admin apabila ada perubahan."
               >
                 <img
-                  src="../img/qm.svg"
+                  src="{{ asset("images/profil/qm.svg") }}"
                   style="width: 16px"
                 />
               </span>
@@ -179,7 +179,7 @@
       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
       crossorigin="anonymous"
     ></script>
-    <script src="../assets/js/script.js"></script>
-    <script src="../assets/js/profile.js"></script>
+    <script src="{{ asset("js/script.js") }}"></script>
+    <script src="{{ asset("js/profile.js") }}"></script>
   </body>
 </html>
