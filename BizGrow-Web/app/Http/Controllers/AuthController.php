6f881ba37283a4917a2e2de2881e715baf31eb0a
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-01-03 13:00:12 +0700

    [REFACTOR] change source of default avatar picture
    Signed-off-by: Ricky Renaldi <109163100+rickrck@users.noreply.github.com>

--- BizGrow-Web/app/Http/Controllers/AuthController.php
+++ BizGrow-Web/app/Http/Controllers/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
             $filePath = $request->file('file_surat_izin')->store('public/surat_izin');
 
             // Foto profile default
-            $defaultProfilePhoto = 'public/images/profil/profile.svg';
+            $defaultProfilePhoto = '/storage/private/default_avatar.jpg';
 
             // Simpan ke tabel umkms
             $umkm = Umkaem::create([
