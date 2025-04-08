bd3c9e520fa68dd2493404932adf740392d62dbd
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-04-08 10:59:20 +0700

    [FEAT] add data umkm page
    Signed-off-by: Ricky Renaldi <109163100+rickrck@users.noreply.github.com>

---
+++ BizGrow-Web/resources/views/admin/data_umkm.blade.php
@@ -0,0 +1,48 @@
+@extends('admin/layout')
+
+@section('title', 'Data UMKM')
+
+@section('header')
+    <div class="d-flex align-items-center">
+        <a href="{{ route('admin.data_umkm') }}">
+            <h3 class="h3 fw-bold mb-0 text-dark">Data UMKM</h3>
+        </a>
+    </div>
+@endsection
+
+@section('cssCustom')
+    {{ asset('css/admin.css') }}
+@endsection
+
+@section('jsCustom')
+    {{ asset('js/admin_data.js') }}
+@endsection
+
+@section('content')
+    <div class="filter-container m-5 d-flex align-items-center">
+        <input type="text" id="umkmNameInput" class="form-control me-3" style="width: 300px;" placeholder="Cari Nama Toko">
+        <button class="btn btn-primary" id="resetButton">
+            <img src="{{ asset('images/refresh.svg') }}" alt="reset-icon"> Reset
+        </button>
+    </div>
+
+    <div class="mx-5 mb-5">
+        <table class="table table-striped text-center align-middle table-borderless">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Durasi(Menit)</th>
+                    <th scope="col">Forecasting Demand</th>
+                    <th scope="col">Buffer Stock</th>
+                    <th scope="col">Akurasi Demand</th>
+                    <th scope="col">Akurasi Stok</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Hapus Data</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        <div id="pagination" class="d-flex justify-content-center align-items-center mt-4"></div>
+    </div>
+@endsection
