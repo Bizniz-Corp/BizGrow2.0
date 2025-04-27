2b33a064478ddad1d90445a50f819298a15dbead
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-04-27 17:04:13 +0700

    [FEAT] Add modal and fix path
    Signed-off-by: Ricky Renaldi <109163100+rickrck@users.noreply.github.com>

--- BizGrow-Web/resources/views/admin/data_umkm.blade.php
+++ BizGrow-Web/resources/views/admin/data_umkm.blade.php
@@ -4,7 +4,7 @@
 
 @section('header')
     <div class="d-flex align-items-center">
-        <a href="{{ route('admin.umkm') }}">
+        <a href="{{ route('admin.data_umkm') }}">
             <h3 class="h3 fw-bold mb-0 text-dark">Data UMKM</h3>
         </a>
     </div>
@@ -19,6 +19,7 @@
 @endsection
 
 @section('content')
+
     <div class="filter-container m-5 d-flex align-items-center">
         <input type="text" id="umkmNameInput" class="form-control me-3" style="width: 300px;" placeholder="Cari Nama Toko">
         <button class="btn btn-primary" id="resetButton">
@@ -44,5 +45,24 @@
             </tbody>
         </table>
         <div id="pagination" class="d-flex justify-content-center align-items-center mt-4"></div>
+    </div>
+
+    <!-- Modal Konfirmasi -->
+    <div class="modal fade" id="deleteConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteConfirmationModalLabel">Konfirmasi Penghapusan</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p id="deleteConfirmationMessage">Apakah Anda yakin ingin menghapus UMKM ini?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-danger" id="confirmDeleteButton">Hapus</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
