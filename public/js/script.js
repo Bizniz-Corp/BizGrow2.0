7353c4e77d835bd9a7c73a21cb93c49c51f95119
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-06-02 11:18:53 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -41,7 +41,6 @@ $(document).ready(function () {
 
     // Menangani klik logout untuk menampilkan modal
     $("#logoutButton").click(function (e) {
-        e.preloginModalentDefault(); // Mencegah navigasi default
 
         // Tampilkan modal konfirmasi logout
         $("#logoutModal").modal("show");
