c79333dfa4261374cc43b8d488d3141a67695323
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-06-13 03:58:14 +0700

    [FIX] comming soon feature

--- resources/views/stok/stok_prediksi_buffer_stok.blade.php
+++ resources/views/stok/stok_prediksi_buffer_stok.blade.php
@@ -1,95 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Input Data Penjualan</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+@section('title', 'Prediksi Buffer Stok')
 
-    <link href="../../assets/css/style_reza.css" rel="stylesheet">
-</head>
+@section('header', 'Prediksi Buffer Stok')
 
-<body>
-    <div class="wrapper">
-        <div class="component-sidebar"></div>
+@section('cssCustom')
+    {{ asset('css/prediksi_profit.css') }}
+@endsection
 
-        <div class="main">
-            <header class="p-3 d-flex justify-content-between align-items-center sticky-top">
-                <h3 class="h3 fw-bold">
-                    Prediksi Permintaan
-                </h3>
-                <img src="../../img/logo.png" alt="bizgrowlogo">
-            </header>
+@section('jsCustom')
+    {{ asset('js/prediksi_profit.js') }}
+@endsection
 
-            <div class="d-flex justify-content-center my-3">
-                <div class="search_select_box">
-                    <select name="" id="productSelect" data-live-search="true" style="width: 500px;">
-                        <option value="Ayam Bakar Nashvilled">Ayam Bakar Nashvilled</option>
-                        <option value="Ayam Goreng Nashvilled">Ayam Goreng Nashvilled</option>
-                        <option value="Ayam Rebus Nashvilled">Ayam Rebus Nashvilled</option>
-                        <option value="Ayam Pepes Nashvilled">Ayam Pepes Nashvilled</option>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row justify-content-center my-3">
-                <div class="d-flex justify-content-center">
-                    <div class="dropdown justify-content-center">
-                        <button class="btn btn-primary dropdown-toggle" type="button" id="pilihWaktuButton"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Pilih Waktu
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="pilihWaktuButton">
-                            <a class="dropdown-item" href="#" id="optionBulan">Bulan</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#" id="optionHari">Hari</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="d-flex justify-content-center my-3">
-                <div style="width: 50%;">
-                    <canvas id="bufferStockChart"></canvas>
-                </div>
-            </div>
-
-            <div>
-                <h5 class="h5 fw-bold d-flex justify-content-center">
-                    Akurasi
-                    </h3>
-                    <div>
-                        <div style="width: 20%; margin: 0 auto;">
-                            <canvas id="percentageChart"></canvas>
-                        </div>
-                    </div>
-            </div>
-
-        </div>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="../../assets/js/script_reza.js"></script>
-    <script src="../../assets/js/komponen_prediksi.js"></script>
-</body>
-
-</html>
+@section('content')
+<div class="d-flex flex-column align-items-center justify-content-center" style="min-height:60vh;">
+    <img src="{{ asset('images/coming-soon.svg') }}" alt="Coming Soon" style="max-width:220px;">
+    <h2 class="mt-4 mb-2">Fitur Segera Hadir!</h2>
+    <p class="text-muted mb-4">Halaman atau fitur ini sedang dalam pengembangan.<br>Silakan kembali lagi nanti.</p>
+    <a href="{{ url()->previous() }}" class="btn btn-primary">Kembali</a>
+</div>
+@endsection
